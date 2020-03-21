7629fd766ef920a28073f7df897e8a0d5c1ac93a
jarrodwatts <35651410+jarrodwatts@users.noreply.github.com>
2020-03-21 17:55:10 +1100

    Added BigPicture Arrays

--- public/sketch.js
+++ public/sketch.js
@@ -1,25 +1,51 @@
 function setup() {
     createCanvas(255, 255);
-    background(255);
+    background(200);
 
     loadJSON('/rainbow', gotRainbow);
 }
 
 function gotRainbow(data) {
+
+    let xCoordsBigPicture = [];
+    let yCoordsBigPicture = [];
+
+    //fill in the dummy arrays with 256 zeros.
+    for (let penis = 0; penis < 256; penis++) {
+        xCoordsBigPicture.push(0);
+        yCoordsBigPicture.push(0);
+    }
+
+    console.log(xCoordsBigPicture);
+    console.log(xCoordsBigPicture.length);
+
+
     //Source: https://www.youtube.com/watch?v=yLuk0twx8Hc
     let drawing = data.drawing;
-
+    //Draw the stuff
     for (let path of drawing) {
         noFill();
         stroke(0);
         strokeWeight(3);
         beginShape();
+
         for (let i = 0; i < path[0].length; i++) {
             let x = path[0][i];
             let y = path[1][i];
             vertex(x, y);
 
+            xCoordsBigPicture[x] = 1;
+            yCoordsBigPicture[y] = 1;
+
         }
+
         endShape();
+
+
+
     }
+    console.log("xCoordsBigPicture:", xCoordsBigPicture);
+    console.log("yCoordsBigPicture:", yCoordsBigPicture);
+
+
 }
